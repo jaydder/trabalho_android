3b2b52aeb31e048f47e84bd66d53b6246b551a7a
LucasFontans <lucasgbfontans@gmail.com>
2023-11-04 17:47:51 -0300

    alteração de model

--- app/src/main/java/com/example/prova/model/User.java
+++ app/src/main/java/com/example/prova/model/User.java
@@ -6,6 +6,25 @@ public class User implements Serializable {
     private String name;
     private String password;
 
+    private String cpf;
+    private int celular;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
     public String getName() {
         return name;
     }

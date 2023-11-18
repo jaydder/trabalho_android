3c4b020005e34b50a35817d125e2079aa8c3403d
jaydder <jaydderfavoravel@gmail.com>
2023-11-18 19:50:40 -0300

    Changed struct from Database

--- app/src/main/java/com/example/prova/db/UserDatabase.java
+++ app/src/main/java/com/example/prova/db/UserDatabase.java
@@ -1,41 +1,44 @@
 package com.example.prova.db;
 
-import android.adservices.customaudience.JoinCustomAudienceRequest;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.service.autofill.UserData;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.example.prova.model.User;
-import com.example.prova.model.UserDates;
-
-import java.util.List;
 
 public class UserDatabase extends SQLiteOpenHelper {
+    User user = null;
+    final static String DATABASE_NAME = "PIX_DB";
+    final static int VERSION = 1;
 
     public UserDatabase(@Nullable Context context) {
-        super(context, "PIX_DB", null, 1);
+        super(context, DATABASE_NAME, null, VERSION);
     }
 
+    String sql;
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "CREATE TABLE user(" +
-                "ID INTEGER PRIMARY KEY," +
+         sql = "CREATE TABLE user(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "name TEXT NOT NULL," +
                 "password TEXT NOT NULL," +
                 "cpf TEXT," +
                 "celular TEXT)";
         sqLiteDatabase.execSQL(sql);
 
+         sql = "CREATE TABLE balance(" +
+                "userID INTEGER," +
+                "balance TEXT  NOT NULL)";
+        sqLiteDatabase.execSQL(sql);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
     }
 
     public void add_user(User user) {
@@ -48,35 +51,48 @@ public class UserDatabase extends SQLiteOpenHelper {
 
     public User login(String name, String password) {
 
-        String sql = "select * from user " +
-                " where name = '" + name + "' and " +
-                " password = '" + password + "'";
+        String sql = "SELECT * FROM user " +
+                "WHERE name = '" + name + "' AND " +
+                "password = '" + password + "'";
 
         Cursor cursor = getReadableDatabase()
                 .rawQuery(sql, null);
-        User usuario = null;
-        Log.d("DBp===", "usuario cirado");
-        for(int i=0; i < cursor.getCount(); i++ ){
-            usuario = new User();
 
-            Log.d("DB",cursor.getString(1));
-            usuario.setId(cursor.getInt(0));
-            usuario.setName(cursor.getString(1));
-            usuario.setPassword(cursor.getString(2));
-            cursor.moveToNext();
+        while (cursor.moveToNext()){
+            user = new User();
+            user.setName(cursor.getString(cursor.getColumnIndexOrThrow("name")));
+            user.setPassword(cursor.getString(cursor.getColumnIndexOrThrow("password")));
+            user.setId(cursor.getInt(cursor.getColumnIndexOrThrow("id")));
         }
-
-        return usuario;
+        return user;
     }
 
-    public void set_keys(int id, String type, String key_value) {
-        String sql = "UPDATE user SET '"+
-                type + "'='" + key_value +
-                "' where ID = "+ id ;
-        Log.i("db", "sql insert usuario: " + sql);
+    public void set_keys(int id, String column, String key_value) {
+        String sql = "UPDATE user SET '" +
+                column + "'='" + key_value +
+                "' where id ='" + id + "'";
         getWritableDatabase().execSQL(sql);
     }
 
+    public void deposit(int id, String value){
+        sql = "INSERT INTO balance(userID, balance)" +
+        "VALUES("+ id +",'"+ value +"');";
 
+        getWritableDatabase().execSQL(sql);
+    }
+
+    public double get_balance(int id) {
+        sql = "SELECT SUM(balance) as TotalBalance FROM balance " +
+              "WHERE userID = '"+ id +"'";
+
+        Cursor cursor = getReadableDatabase()
+                .rawQuery(sql, null);
+
+        double total = 0;
+        if (cursor.moveToFirst()) {
+            total = cursor.getDouble(cursor.getColumnIndexOrThrow("TotalBalance"));
+        }
+        return total;
+    }
 }
 
